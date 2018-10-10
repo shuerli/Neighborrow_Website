06dfe5bbf9105935973f24f2101ef6958e991251
Zi Wang <zikevin.wang@mail.utoronto.ca>
2018-10-10 16:27:13 -0400

    database table 'feedback_to_lender' created in migration file

---
+++ db/migrate/20181010201202_create_feedback_to_lenders.rb
@@ -0,0 +1,13 @@
+class CreateFeedbackToLenders < ActiveRecord::Migration[5.2]
+  def change
+    create_table :feedback_to_lenders do |t|
+			t.integer :ftl_id
+			t.integer :request_id
+			t.integer :rate
+			t.text :tag, array:true
+			t.integer :credit
+			t.text :comment 
+      t.timestamps
+    end
+  end
+end
