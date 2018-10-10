f439c384ff62fb9634d3f726aa7ea2737bd71c0b
Zi Wang <zikevin.wang@mail.utoronto.ca>
2018-10-10 11:37:13 -0400

    migration file of 'accounts' table updated

--- db/migrate/20181010144455_create_accounts.rb
+++ db/migrate/20181010144455_create_accounts.rb
@@ -1,11 +1,11 @@
 class CreateAccounts < ActiveRecord::Migration[5.2]
   def change
     create_table :accounts do |t|
-			t.text :email
-			t.text :role
-			t.text :password
-			t.text :salt
-			t.text :status
+			t.text :email, null: false
+			t.text :role, null: false
+			t.text :password, null: false
+			t.text :salt, null: false
+			t.text :status, null: false
       t.timestamps
 		end
   end
