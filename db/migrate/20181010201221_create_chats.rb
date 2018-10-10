39b7f7b9997640ea715e56d182070012502743cb
Zi Wang <zikevin.wang@mail.utoronto.ca>
2018-10-10 16:34:47 -0400

    database table 'chat' created in migration file

---
+++ db/migrate/20181010201221_create_chats.rb
@@ -0,0 +1,16 @@
+class CreateChats < ActiveRecord::Migration[5.2]
+  def change
+    create_table :chats do |t|
+			t.integer :chat_id
+			t.integer :request_id
+			t.integer :report_id
+			t.timestamp :time_submitted
+			t.timestamp :time_closed
+			t.text :sender
+			t.text :receiver
+			t.text :content
+      t.timestamps
+    end
+  end
+end
+
