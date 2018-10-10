9088667443b84e4b853f1597a99c93944db6cab0
Zi Wang <zikevin.wang@mail.utoronto.ca>
2018-10-10 11:39:09 -0400

    database table 'profile' created in migration file

---
+++ db/migrate/20181010151725_create_profiles.rb
@@ -0,0 +1,24 @@
+class CreateProfiles < ActiveRecord::Migration[5.2]
+  def change
+		create_table :profiles do |t|
+			t.text :email
+			t.text :first_name
+			t.text :middle_name
+			t.text :last_name
+			t.text :display_name, null: false
+			t.text :phone_number
+			t.text :gender
+			t.text :language, array: true, null: false
+			t.text :country, default: 'Canada'
+			t.text :facebook
+			t.text :google
+			t.text :wechat
+			t.text :twitter
+			t.text :avatar_url, null: false
+			t.text interest, array: true
+      t.timestamps
+    end
+  end
+end
+
+
