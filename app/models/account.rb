75b0cd3a3858599eb4847ae5d64be62052fe768f
Zeyu Feng <zeyu.feng@mail.utoronto.ca>
2018-10-15 14:04:42 -0400

    Added validation for account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,9 @@
 class Account < ApplicationRecord
+	ROLE_OPTIONS = %w(user admin)
+	STATUS_OPTIONS = %w(created active suspended banned)
+
+	validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
+	validates :role, presence: true, :inclusion => {:in => ROLE_OPTIONS}
+	validates :status, presence: true, :inclusion => {:in => STATUS_OPTIONS}
+	validates :password, :salt, :presence => true
 end
