f79d04f72e42f2e832bb9bc7ab8f5476f14c2122
Zi Wang <zikevin.wang@mail.utoronto.ca>
2018-11-06 17:38:25 -0500

    request#show in progress, switch cases and sql query sample generated

--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -1,10 +1,5 @@
 class RequestController < ApplicationController
 	layout false
-	#def list
-	#	requests = Request.all
-	#	puts requests.inspect
-	#	render :json => {:name => requests}
-	#end
 
 	def page_borrowed
 	end
@@ -13,15 +8,49 @@ class RequestController < ApplicationController
 	end
 
 	def show
-		render :json => {:status => nil}
+		case params[:type]
+		when 'borrowed'
+			case params[:sorted_by]
+			when 'default'
+
+			when 'update_time'
+				user_email = Account.find_by(id: 1).email
+				borrowed_requests = ActiveRecord::Base.connection.exec_query("SELECT * FROM Requests,Items WHERE Requests.borrower = 'activeuser@gmail.com' AND Requests.item_id=Items.id ORDER BY Requests.updated_at DESC;")
+				render :json => {:status => 200, :result => borrowed_requests}
+			when 'item_name'
+				user_email = Account.find_by(id: 1).email
+				borrowed_requests = ActiveRecord::Base.connection.exec_query("SELECT * FROM Requests,Items WHERE Requests.borrower = 'activeuser@gmail.com' AND Requests.item_id=Items.id ORDER BY Items.name ASC;")
+				render :json => {:status => 200, :result => borrowed_requests}
+			when 'lender_name'
+
+
+			end
+		when'lended'
+			case params[:sorted_by]
+			when 'time'
+
+			when 'itemName'
+
+			when 'status'
+
+			end
+		else
+			render :json => {:status => 404}
+		end
+		
+
 	end
 
 	def create
+		
 		render :json => {:status => nil}
 	end
 
 	def update
+
 		render :json => {:status => nil}
 	end
+
+
 
 end
