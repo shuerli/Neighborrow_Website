003414b15c2e62926ec13b1a0383aaefcbbe2ff3
Zeyu Feng <zeyu.feng@mail.utoronto.ca>
2018-11-06 16:41:51 -0500

    Fixed a problem the arrows not showing correctly on calendar when adding new items

--- public/items.js
+++ public/items.js
@@ -1,35 +1,6 @@
+// For item removing button on top right of item image
 $('.img-wrap .close').on('click', function() {
   var id = $(this).closest('.img-wrap').find('img').data('id');
   alert('remove picture: ' + id);
 });
-
-var myCalendar;
-function doOnLoad() {
-  myCalendar = new dhtmlXCalendarObject(["date_from","date_to"]);
-  myCalendar.setDate("2018-12-01");
-  myCalendar.hideTime();
-  
-  // init values
-  var t = new Date();
-  byId("date_from").value = "2018-12-01";
-  byId("date_to").value = "2018-12-01";
-}
-
-function setSens(id, k) {
-  // update range
-  if (k == "min") {
-    myCalendar.setSensitiveRange(byId(id).value, null);
-  } else {
-    myCalendar.setSensitiveRange(null, byId(id).value);
-  }
-}
-function byId(id) {
-  return document.getElementById(id);
-}
-
-
-
-$(document).ready(function(){
-  doOnLoad();
-})
 
