f76e972478405ac5c4297c00e5b2e5d1437958f5
lillizhuo <qingqing.zhuo@mail.utoronto.ca>
2018-11-06 22:06:37 -0500

    add omniauth initializer

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :google_oauth2, "744444716406-m0jlfmlkif1lqk6mpfklauvrtnvg312c.apps.googleusercontent.com", "8kiydNqyfro1LdX9Vg1XPKSB"
+end
