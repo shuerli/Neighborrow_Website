449ce1dee4e602db1e1ef19de41116fb64ff16dc
Zeyu Feng <zeyu.feng@mail.utoronto.ca>
2018-11-10 20:00:34 -0500

    changed user item layout using col-2 to improve grid view. Changed style for add new item button.Added image url for user registered items in seeds for testing.

--- public/items.js
+++ public/items.js
@@ -5,14 +5,14 @@ $('.img-wrap .close').on('click', function() {
 });
 
 
-
 //Receive data from controller when page loads
 $(document).ready(function() {
   //Add image and name for lent items
   $.get("/user_item?type=lent",function(data){
       for (var i=0;i<data.result.length;i++){
         var divImage = document.createElement('div');
-        divImage.innerHTML = '<h2><img src="imgplaceholder.gif" alt="my lent item" width="150" height="150" ></h2>\
+        divImage.className = "col-2"
+        divImage.innerHTML = '<h2><img src="' + data.result[i].photo_url + '" alt="imgplaceholder.gif" class="img-fluid img-thumbnail"></h2>\
                             <p>' + data.result[i].name + '</p>';
         document.getElementById("lent-items").appendChild(divImage);
       }
@@ -22,14 +22,17 @@ $(document).ready(function() {
     for (var i=0;i<data.result.length;i++){
       var divImage = document.createElement('div');
       divImage.className = "img-wrap";
+      divImage.className = "col-2"
       divImage.innerHTML = '<span class="close">&times;</span>\
-                          <h2><img src="' + data.result[i].photo_url + '" alt="my registered item" width="150" height="150"></h2>\
+                          <h2><img src="' + data.result[i].photo_url + '" alt="imgplaceholder.gif" class="img-fluid img-thumbnail" ></h2>\
                             <p>' + data.result[i].name + '</p>';
       document.getElementById("registered-items").appendChild(divImage);
     }
     var buttonAddImage = document.createElement('button');
       buttonAddImage.className = "btn btn-secondary";
+      buttonAddImage.className = "col-2"
       buttonAddImage.innerHTML = '<i class="fa fa-plus fa-5x"></i>'
+      buttonAddImage.setAttribute("style","margin:50px 50px 50px 50px;");
       document.getElementById("registered-items").appendChild(buttonAddImage);
   });
 });
