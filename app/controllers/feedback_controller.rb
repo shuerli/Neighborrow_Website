0cce3e8f3d38430ab269a0808d0311602bd917c5
Zi Wang <zikevin.wang@mail.utoronto.ca>
2018-11-11 19:29:24 -0500

    search query for feedbacks of the designated user completed

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,7 +1,7 @@
 class FeedbackController < ApplicationController
 
 	def create_toBorrower
-		
+
 	end
 
 	def create_toLender
@@ -11,11 +11,19 @@ class FeedbackController < ApplicationController
 	def show
 		case params[:scope]
 		when 'user'
-		
-		when 'feedback_to_lender'
-		
-		when 'feedback_to_borrower'
-		
+			as_lender_query = "SELECT Feedback_to_lenders.* FROM Feedback_to_lenders, Requests, Items WHERE Feedback_to_lenders.request_id = Requests.id AND Requests.item_id = Items.id AND Items.owner = '';" 
+			as_borrower_query = "SELECT Feedback_to_borrowers.* FROM Feedback_to_borrowers, Requests WHERE Requests.borrower = '' AND Requests.id = Feedback_to_borrowers.request_id;"
+			as_lender_rate_query = "SELECT AVG(Feedback_to_lenders.rate) FROM Feedback_to_lenders, Requests, Items WHERE Feedback_to_lenders.request_id = Requests.id AND Requests.item_id = Items.id AND Items.owner = '';";
+			as_borrower_rate_query = "SELECT AVG(Feedback_to_borrowers.rate) FROM Feedback_to_borrowers, Requests WHERE Requests.borrower = '' AND Requests.id = Feedback_to_borrowers.request_id;";
+			feedback_to_lender_list = ActiveRecord::Base.connection.exec_query(as_lender_query)
+			feedback_to_borrower_list = ActiveRecord::Base.connection.exec_query(as_borrower_query)
+			as_lender_rate = ActiveRecord::Base.connection.exec_query(as_lender_rate_query)
+			as_borrower_rate = ActiveRecord::Base.connection.exec_query(as_borrower_rate_query)
+			render :json => {:status => 200, :as_lender => feedback_to_lender_list, :as_borrower => feedback_to_borrower_list, :rate_as_lender => as_lender_rate, :rate_as_borrower => as_borrower_rate}
+		when 'items'
+
+		else
+			render :json => {:status => 404}
 		end
 	end
 
