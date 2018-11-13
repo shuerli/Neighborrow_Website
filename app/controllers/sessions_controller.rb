2fc420934cbca84f2fdfd88045f4fc4e8986c2bc
lillizhuo <qingqing.zhuo@mail.utoronto.ca>
2018-11-12 22:20:57 -0500

    Email verification is now working.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
       if user && user.authenticate(params[:session][:email], params[:session][:password])
       if user.email_confirmed
           log_in user
-          redirect_back_or user
+          redirect_to user
       else
          flash.now[:error] = 'Please activate your account by following the instructions in the account confirmation email you received to proceed'
          render 'login'
