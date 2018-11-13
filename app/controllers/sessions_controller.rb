24a7e354a74bfc7d2d4e75281b2a3e54326520b3
lillizhuo <qingqing.zhuo@mail.utoronto.ca>
2018-11-12 21:30:51 -0500

    fix errors from merging

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,9 +10,14 @@ class SessionsController < ApplicationController
   def create
       user = Account.find_by(email: params[:session][:email])
       if user && user.authenticate(params[:session][:email], params[:session][:password])
+      if user.email_confirmed
           log_in user
           redirect_to user
-    else
+      else
+         flash.now[:error] = 'Please activate your account by following the instructions in the account confirmation email you received to proceed'
+         render 'login'
+      end
+      else
           flash.now[:danger] = 'Invalid email/password combination'
           render 'login'
       end
