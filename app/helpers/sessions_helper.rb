978881609015dd5af1c652419cc5327d9554b377
gloriali2011 <geling.li@mail.utoronto.ca>
2018-11-20 23:00:44 -0500

    Add session control for profile: Only allow user to edit and see their own account's profile when they are logged in. If trying to access another account's profile page, return a 403 error page.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,5 +22,15 @@ module SessionsHelper
         !current_user.nil?
     end
     
+    # Returns true if the user is logged in as UserID, false otherwise.
+    def logged_in_as_user? (user_id)
+        if current_user.nil?
+          return false
+        elsif current_user.id != user_id
+          return false
+        else
+          return true
+        end
+    end
     
 end
