aa6b0ff9bd7409b9a3dc785cfe99d41334bba4d7
Zi Wang <zikevin.wang@mail.utoronto.ca>
2018-11-25 22:03:36 -0500

    search function improved with spelling correction

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,22 +29,14 @@ class ItemsController < ApplicationController
 		# Case 2: Keyword of Users (display_name, email)
 		# Case 3: Keyword of Locations (current_city)
 
-		#require 'gingerice'
-		#text = 'textbeek'
-		#parser = Gingerice::Parser.new
-		#result = parser.parse text
-		#puts result["result"]
-
 		############################ User Check ##################################
 		# Situation 1: Email
 		# Situation 2: Display_name
-		parameters = "display_name = '"+keyword+"' "
 		email_check = (keyword =~ /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/)
 		if(email_check == 0)
-			parameters = parameters + "OR email = '"+keyword+"';"
+			search_byUserEmail = ActiveRecord::Base.connection.exec_query("SELECT email, display_name, gender, language, country, avatar_url FROM Profiles WHERE email = '"+keyword+"';")
 		end
-		search_forUser = ActiveRecord::Base.connection.exec_query("SELECT email, display_name, gender, language, country, avatar_url FROM Profiles WHERE "+parameters)
-
+		search_byUserName = ActiveRecord::Base.connection.exec_query("SELECT email, display_name, gender, language, country, avatar_url FROM Profiles WHERE display_name = '"+keyword+"';")
 		############################ Items Check ##################################
 		# Situation 1: ISBN
 		if(keyword.to_i.to_s == keyword && (keyword.length==13 || keyword.length==10))
@@ -57,8 +49,16 @@ class ItemsController < ApplicationController
 		end
 
 		# Situation 3: Name & Brand
-		search_byItemNameAndBrand = ActiveRecord::Base.connection.exec_query("SELECT * FROM Items WHERE name LIKE '%"+keyword+"%' OR brand = '%"+keyword+"%';")
+		search_byItemNameAndBrand = ActiveRecord::Base.connection.exec_query("SELECT * FROM Items WHERE LOWER(name) LIKE LOWER('%"+keyword+"%') OR LOWER(brand) = LOWER('%"+keyword+"%');")
+		puts :search_byItemNameAndBrand.size
+		if(0)
+			require 'gingerice'
+			parser = Gingerice::Parser.new
+			correction = parser.parse(keyword)
+			correction_result = correction["result"].downcase
+			search_byCorrection = ActiveRecord::Base.connection.exec_query("SELECT * FROM Items WHERE LOWER(name) LIKE LOWER('%"+correction_result+"%') OR LOWER(brand) = LOWER('%"+correction_result+"%');")
+		end
 		
-		render :json => {:status => 200, :result_user => search_forUser, :result_itemISBN => search_byISBN, :result_itemID => search_byItemID, :result_itemNameBrand => search_byItemNameAndBrand}
+		render :json => {:status => 200, :result_userEmail => search_byUserEmail, :result_userName => search_byUserName, :result_itemISBN => search_byISBN, :result_itemID => search_byItemID, :result_itemNameBrand => search_byItemNameAndBrand, :corrected_keyword => correction_result, :result_correctedKeyword => search_byCorrection}
 	end
 end
