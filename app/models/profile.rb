1ac8a03793972f62a16fb6fc0058984c4bc23790
gloriali2011 <geling.li@mail.utoronto.ca>
2018-11-26 00:55:51 -0500

    only update the profile picture if the uploaded document is an image type

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,7 @@
 class Profile < ApplicationRecord
     validates :email, :display_name, :presence => true
     has_one_attached :avatar
+#    validate :avatar_picture_format
     after_commit :add_default_avatar, on: [:create, :update]
     
     private def avatar_picture_format
@@ -16,7 +17,32 @@ class Profile < ApplicationRecord
         self.avatar.attach(io: File.open(Rails.root.join("app", "assets", "images", "avatar.jpg")), filename: 'avatar.jpg' , content_type: "image/jpg")
       end
     end
+    
+#    private def add_default_avatar_force
+#       self.avatar.attach(io: File.open(Rails.root.join("app", "assets", "images", "avatar.jpg")), filename: 'avatar.jpg' , content_type: "image/jpg")
+#    end
     # validates doesn't work right now:
-    validates :avatar, blob: { content_type: :image } # supported options: :image, :audio, :video, :text
-   
+#    validates :avatar, blob: { content_type: :image } # supported options: :image, :audio, :video, :text
+#    validates :avatar, blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg'], size_range: 1..5.megabytes }
+    
+#    validates :avatar, file_size: { less_than_or_equal_to: 100.kilobytes },
+#                       file_content_type: { allow: ['image/jpeg', 'image/png'] }
+#    validate :avatar_validation
+#    
+#    def avatar_validation
+#      if avatar.attached?
+#        if avatar.blob.byte_size > 1000000
+#          avatar.purge
+#          errors[:base] << 'Too big'
+#          add_default_avatar_force
+#        elsif !avatar.blob.content_type.starts_with?('image/')
+#          logo.purge
+#          errors[:base] << 'Wrong format'
+#          add_default_avatar_force
+#        end
+#      end
+#    end
+    
+  
+
 end
