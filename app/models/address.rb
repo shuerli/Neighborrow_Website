f0450bc5747008bd84c2364cf6bf968efc7e5b2a
Zeyu Feng <zeyu.feng@mail.utoronto.ca>
2018-11-26 22:43:39 -0500

    Changed seed file, item and category dependencies and model accordingly

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,4 @@
 class Address < ApplicationRecord
-    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
     validates :address_line1, :city, :province, :country, :postal_code, :presence => true
-    belongs_to :account
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,3 @@
 class Item < ApplicationRecord
 validates :owner, :condition, :time_start, :time_end, :name, :description, :presence => true
-   belongs_to :category
 end
