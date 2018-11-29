179b1d1b1e9e929f1b404bf2afaf78dbbdee5bcb
gloriali2011 <geling.li@mail.utoronto.ca>
2018-11-28 21:41:59 -0500

    Now we have user profile and user's listed items in the public profile page.

--- app/controllers/public_profiles_controller.rb
+++ app/controllers/public_profiles_controller.rb
@@ -5,7 +5,7 @@ class PublicProfilesController < ApplicationController
       @profile = Profile.find_by_email(@account[:email])
         
       @borrower_rate = ActiveRecord::Base.connection.exec_query("SELECT AVG(Feedback_to_borrowers.rate) AS borrower_avg FROM Requests,Feedback_to_borrowers WHERE Requests.borrower = '"+@account.email+"' AND Requests.id = Feedback_to_borrowers.request_id;")
-        
+      @items = Item.where(:owner => @account.email)
   end  
   
   
