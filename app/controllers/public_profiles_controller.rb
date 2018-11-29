7bc05055766f46a67a46257f1bc21a527c431bd7
gloriali2011 <geling.li@mail.utoronto.ca>
2018-11-28 19:51:07 -0500

    add a public profiles controller for displaying user's profile to the public

---
+++ app/controllers/public_profiles_controller.rb
@@ -0,0 +1,12 @@
+class PublicProfilesController < ApplicationController
+  
+  def show
+      @account = Account.find(params[:id])
+      @profile = Profile.find_by_email(@account[:email])
+        
+      @borrower_rate = ActiveRecord::Base.connection.exec_query("SELECT AVG(Feedback_to_borrowers.rate) AS borrower_avg FROM Requests,Feedback_to_borrowers WHERE Requests.borrower = '"+@account.email+"' AND Requests.id = Feedback_to_borrowers.request_id;")
+        
+  end  
+  
+  
+end
