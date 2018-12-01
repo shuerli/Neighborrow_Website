d11ece47fd1a1df2fbc8339dc7b02d622f21c8d9
gloriali2011 <geling.li@mail.utoronto.ca>
2018-12-01 14:39:05 -0500

    make the cards in the same row equal height

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,17 +3,17 @@ class Profile < ApplicationRecord
     has_one_attached :avatar
 #    validate :avatar_picture_format
     after_commit :add_default_avatar, on: [:create, :update]
-    
-    private def avatar_picture_format
-      return unless avatar.attached?
-      return if avatar.blob.content_type.start_with? 'image/'
-      avatar.purge_later
-      errors.add(:avatar, 'needs to be an image')
-      add_default_avatar_force
-    end
-    
+#    
+#    private def avatar_picture_format
+#      return unless avatar.attached?
+#      return if avatar.blob.content_type.start_with? 'image/'
+#      avatar.purge_later
+#      errors.add(:avatar, 'needs to be an image')
+#      add_default_avatar_force
+#    end
+#    
     private def add_default_avatar
-      unless self.avatar.attached?
+      unless avatar.attached?
         self.avatar.attach(io: File.open(Rails.root.join("app", "assets", "images", "avatar.jpg")), filename: 'avatar.jpg' , content_type: "image/jpg")
       end
     end
