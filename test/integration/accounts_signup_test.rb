4cd9588c3ac5d4ea6bb94fa957c4286bfb83c28a
lillizhuo <qingqing.zhuo@mail.utoronto.ca>
2018-12-01 18:42:26 -0500

    testing #not working

---
+++ test/integration/accounts_signup_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class AccountsSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+   test "valid signup information" do
+       get signup_path
+       assert_difference 'User.count', 1 do
+           post accounts_path, params: { account: {email: "user@example.com", password:"password",password_confirmation: "password" } }
+       end
+       follow_redirect!
+       assert_template 'accounts/show'
+   end
+
+end
