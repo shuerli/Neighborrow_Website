3bfda0f14541719d5134b61eae5f6ca0bef034c1
Zi Wang <zikevin.wang@mail.utoronto.ca>
2018-12-02 18:08:30 -0500

    condition filter in search result completed

--- app/assets/javascripts/search_result.js
+++ app/assets/javascripts/search_result.js
@@ -88,6 +88,17 @@ const state_lookup = {
   YT: "Yukon Territory"
 };
 
+let filterOut_status = {
+  "Brand New": false,
+  "Like New": false,
+  "Very Good": false,
+  Good: false,
+  Adequate: false,
+  Defective: false
+};
+
+let cache = null;
+
 $(document).ready(function() {
   document.getElementById("itemISBN_section").style.display = "None";
   document.getElementById("itemNameBrand_section").style.display = "None";
@@ -95,13 +106,13 @@ $(document).ready(function() {
   document.getElementById("no_results_section").style.display = "None";
   let urlObj = new URL(window.location.href);
   let keyword = urlObj.searchParams.get("keyword");
-	let city = urlObj.searchParams.get("city");
-	let country = urlObj.searchParams.get("country");
+  let city = urlObj.searchParams.get("city");
+  let country = urlObj.searchParams.get("country");
   let province = urlObj.searchParams.get("province");
-	if(country==="USA"||country==="Canada"){
-		province = state_lookup[urlObj.searchParams.get("province")]
-	}
-  
+  if (country === "USA" || country === "Canada") {
+    province = state_lookup[urlObj.searchParams.get("province")];
+  }
+
   fetch(
     "/result_api?keyword=" +
       keyword +
@@ -116,10 +127,12 @@ $(document).ready(function() {
       return response.json();
     })
     .then(data => {
-			console.log(data)
-			document.getElementById("current_city").innerText = data.given_city
-			document.getElementById("current_province").innerText = data.given_province
-			document.getElementById("current_country").innerText = data.given_country
+			console.log(data);
+			cache = data;
+      document.getElementById("current_city").innerText = data.given_city;
+      document.getElementById("current_province").innerText =
+        data.given_province;
+      document.getElementById("current_country").innerText = data.given_country;
       if (data.result_userEmail.result !== null) {
         if (data.result_userEmail.result.length !== 0) {
           user_render(data.result_userEmail);
@@ -132,7 +145,43 @@ $(document).ready(function() {
 		}*/
 
       item_render(data);
-    });
+    }).then(()=>{
+			$("#brandnew_checkbox").on("change", function() {
+				if ($(this).is(":checked")) filterOut_status["Brand New"] = false;
+				else filterOut_status["Brand New"] = true;
+				item_render(cache);
+			});
+		
+			$("#likenew_checkbox").on("change", function() {
+				if ($(this).is(":checked")) filterOut_status["Like New"] = false;
+				else filterOut_status["Like New"] = true;
+				item_render(cache);
+			});
+		
+			$("#verygood_checkbox").on("change", function() {
+				if ($(this).is(":checked")) filterOut_status["Very Good"] = false;
+				else filterOut_status["Very Good"] = true;
+				item_render(cache);
+			});
+		
+			$("#good_checkbox").on("change", function() {
+				if ($(this).is(":checked")) filterOut_status["Good"] = false;
+				else filterOut_status["Good"] = true;
+				item_render(cache);
+			});
+		
+			$("#adequate_checkbox").on("change", function() {
+				if ($(this).is(":checked")) filterOut_status["Adequate"] = false;
+				else filterOut_status["Adequate"] = true;
+				item_render(cache);
+			});
+		
+			$("#defective_checkbox").on("change", function() {
+				if ($(this).is(":checked")) filterOut_status["Defective"] = false;
+				else filterOut_status["Defective"] = true;
+				item_render(cache);
+			});
+		});
 });
 
 let user_render = userBlock => {
@@ -168,6 +217,11 @@ let user_render = userBlock => {
 };
 
 let item_render = itemList => {
+
+	document.getElementById("itemISBN_section_content").innerHTML = "";
+	document.getElementById("itemNameBrand_section_content").innerHTML = "";
+	document.getElementById("itemISBN_section_content").innerHTML = "";
+
   let totalCount = 0;
   if (
     itemList.result_itemISBN !== null &&
@@ -175,19 +229,24 @@ let item_render = itemList => {
   ) {
     document.getElementById("isbn_result_length").innerText =
       itemList.result_itemISBN.length;
-		document.getElementById("isbn_keyword").innerText = itemList.search_keyword;
+    document.getElementById("isbn_keyword").innerText = itemList.search_keyword;
     for (let i = 0; i < itemList.result_itemISBN.length; i++) {
-
-			let history_count = 0
-			if(itemList.search_byISBN_requestsCount!==null){
-				let temp = itemList.search_byISBN_requestsCount.filter(x=>x.itemID==itemList.result_itemISBN[i].itemID)
-				if(temp.length>0)
-				history_count = temp[0]["count"]
-			}
-			let lenderRate = 0;
-			if(itemList.search_byISBN_lenderRate!==null)
-				lenderRate = itemList.search_byISBN_lenderRate[itemList.result_itemISBN[i].ownerID]
-      document.getElementById("itemISBN_section").innerHTML +=
+			if(filterOut_status[itemList.result_itemISBN[i].itemCondition])
+			continue
+      let history_count = 0;
+      if (itemList.search_byISBN_requestsCount !== null) {
+        let temp = itemList.search_byISBN_requestsCount.filter(
+          x => x.itemID == itemList.result_itemISBN[i].itemID
+        );
+        if (temp.length > 0) history_count = temp[0]["count"];
+      }
+      let lenderRate = 0;
+      if (itemList.search_byISBN_lenderRate !== null)
+        lenderRate =
+          itemList.search_byISBN_lenderRate[
+            itemList.result_itemISBN[i].ownerID
+          ];
+      document.getElementById("itemISBN_section_content").innerHTML +=
         '<div class="col-lg-3 col-md-4 col-sm-6"><div class="card"><span class="item_detail_span" style="cursor:pointer" onclick="location.href = \'/items/' +
         itemList.result_itemISBN[i].itemID +
         '\'"><img class="card-img-top img-thumbnail" style="width:100%;height:250px; border-top-width: 0px; border-right-width: 0px;border-bottom-width: 0px;border-left-width: 0px;padding-top: 0px;padding-right: 0px;padding-bottom: 0px;padding-left: 0px;" src="' +
@@ -196,13 +255,21 @@ let item_render = itemList => {
         condition_pill[itemList.result_itemISBN[i].itemCondition] +
         '<div class="card-body" style="padding-left: 10px;padding-bottom: 10px;padding-top: 10px;"><div style="line-height: 1.5em; height: 3em;overflow: hidden;"><strong class="card-title text-dark">' +
         itemList.result_itemISBN[i].name +
-        '</strong></div><small class="text-dark"><u>Borrowed by '+history_count+' user(s)</u></small></div><hr style="margin-top:0;margin-bottom:0;" /></span><div class="card-body user_detail_span" onclick="location.href = \'/public_profiles/'+itemList.result_itemISBN[i].ownerID+'\';" style="cursor: pointer;padding-left: 10px;padding-bottom: 2px;padding-top: 5px;">' +
+        '</strong></div><small class="text-dark"><u>Borrowed by ' +
+        history_count +
+        ' user(s)</u></small></div><hr style="margin-top:0;margin-bottom:0;" /></span><div class="card-body user_detail_span" onclick="location.href = \'/public_profiles/' +
+        itemList.result_itemISBN[i].ownerID +
+        '\';" style="cursor: pointer;padding-left: 10px;padding-bottom: 2px;padding-top: 5px;">' +
         '<span><img class="img-thumbnail" style="width:30px; height:30px; border-radius: 50%; padding-top:0; padding-bottom:0; padding-left:0; padding-right:0;" src="' +
-        itemList.search_byISBN_lenderPhoto[itemList.result_itemISBN[i].ownerID] +
+        itemList.search_byISBN_lenderPhoto[
+          itemList.result_itemISBN[i].ownerID
+        ] +
         '"/>&nbsp; <label class="text-muted">' +
         itemList.result_itemISBN[i].ownerName +
         "</label>" +
-        '<label class="pull-right text-warning" style="margin-top:1.5px;"><i class="fa fa-star"></i> '+lenderRate.toFixed(1)+'</label></span></div></div></div>';
+        '<label class="pull-right text-warning" style="margin-top:1.5px;"><i class="fa fa-star"></i> ' +
+        lenderRate.toFixed(1) +
+        "</label></span></div></div></div>";
       totalCount++;
     }
     totalCount++;
@@ -216,19 +283,25 @@ let item_render = itemList => {
     document.getElementById("namebrand_result_count").innerText =
       itemList.result_itemNameBrand.length;
     document.getElementById("namebrand_keyword").innerText =
-			itemList.search_keyword;
-		
+      itemList.search_keyword;
+
     for (let i = 0; i < itemList.result_itemNameBrand.length; i++) {
-			let history_count = 0
-			if(itemList.search_byNameBrand_requestsCount!==null){
-				let temp = itemList.search_byNameBrand_requestsCount.filter(x=>x.itemID==itemList.result_itemNameBrand[i].itemID)
-				if(temp.length>0)
-				history_count = temp[0]["count"]
-			}
-			let lenderRate = 0;
-				if(itemList.search_byItemNameAndBrand_lenderRate!==null)
-					lenderRate = itemList.search_byItemNameAndBrand_lenderRate[itemList.result_itemNameBrand[i].ownerID]
-      document.getElementById("itemNameBrand_section").innerHTML +=
+			if(filterOut_status[itemList.result_itemNameBrand[i].itemCondition])
+				continue
+      let history_count = 0;
+      if (itemList.search_byNameBrand_requestsCount !== null) {
+        let temp = itemList.search_byNameBrand_requestsCount.filter(
+          x => x.itemID == itemList.result_itemNameBrand[i].itemID
+        );
+        if (temp.length > 0) history_count = temp[0]["count"];
+      }
+      let lenderRate = 0;
+      if (itemList.search_byItemNameAndBrand_lenderRate !== null)
+        lenderRate =
+          itemList.search_byItemNameAndBrand_lenderRate[
+            itemList.result_itemNameBrand[i].ownerID
+          ];
+      document.getElementById("itemNameBrand_section_content").innerHTML +=
         '<div class="col-lg-3 col-md-4 col-sm-6"><div class="card"><span class="item_detail_span" style="cursor:pointer" onclick="location.href = \'/items/' +
         itemList.result_itemNameBrand[i].itemID +
         '\'"><img class="card-img-top img-thumbnail" style="width:100%;height:250px; border-top-width: 0px; border-right-width: 0px;border-bottom-width: 0px;border-left-width: 0px;padding-top: 0px;padding-right: 0px;padding-bottom: 0px;padding-left: 0px;" src="' +
@@ -237,13 +310,21 @@ let item_render = itemList => {
         condition_pill[itemList.result_itemNameBrand[i].itemCondition] +
         '<div class="card-body" style="padding-left: 10px;padding-bottom: 10px;padding-top: 10px;"><div style="line-height: 1.5em; height: 3em;overflow: hidden;"><strong class="card-title text-dark">' +
         itemList.result_itemNameBrand[i].name +
-        '</strong></div><small class="text-dark"><u>Borrowed by '+history_count+' user(s)</u></small></div><hr style="margin-top:0;margin-bottom:0;" /></span><div class="card-body user_detail_span" onclick="location.href = \'/public_profiles/'+itemList.result_itemNameBrand[i].ownerID+'\';" style="cursor: pointer;padding-left: 10px;padding-bottom: 2px;padding-top: 5px;">' +
+        '</strong></div><small class="text-dark"><u>Borrowed by ' +
+        history_count +
+        ' user(s)</u></small></div><hr style="margin-top:0;margin-bottom:0;" /></span><div class="card-body user_detail_span" onclick="location.href = \'/public_profiles/' +
+        itemList.result_itemNameBrand[i].ownerID +
+        '\';" style="cursor: pointer;padding-left: 10px;padding-bottom: 2px;padding-top: 5px;">' +
         '<span><img class="img-thumbnail" style="width:30px; height:30px; border-radius: 50%; padding-top:0; padding-bottom:0; padding-left:0; padding-right:0;" src="' +
-        itemList.search_byItemNameAndBrand_lenderPhoto[itemList.result_itemNameBrand[i].ownerID] +
+        itemList.search_byItemNameAndBrand_lenderPhoto[
+          itemList.result_itemNameBrand[i].ownerID
+        ] +
         '"/>&nbsp; <label class="text-muted">' +
         itemList.result_itemNameBrand[i].ownerName +
         "</label>" +
-        '<label class="pull-right text-warning" style="margin-top:1.5px;"><i class="fa fa-star"></i> '+lenderRate.toFixed(1)+'</label></span></div></div></div>';
+        '<label class="pull-right text-warning" style="margin-top:1.5px;"><i class="fa fa-star"></i> ' +
+        lenderRate.toFixed(1) +
+        "</label></span></div></div></div>";
       totalCount++;
     }
     document.getElementById("itemNameBrand_section").style.display = "";
@@ -262,16 +343,22 @@ let item_render = itemList => {
       document.getElementById("corrected_keyword").innerText =
         itemList.corrected_keyword;
       for (let i = 0; i < itemList.result_correctedKeyword.length; i++) {
-				let history_count = 0
-				if(itemList.search_byCorrection_requestsCount!==null){
-					let temp = itemList.search_byCorrection_requestsCount.filter(x=>x.itemID==itemList.result_correctedKeyword[i].itemID)
-					if(temp.length>0)
-					history_count = temp[0]["count"]
-				}
-				let lenderRate = 0;
-				if(itemList.search_byCorrection_lenderRate!==null)
-					lenderRate = itemList.search_byCorrection_lenderRate[itemList.result_correctedKeyword[i].ownerID]
-        document.getElementById("itemNameBrand_section").innerHTML +=
+				if(filterOut_status[itemList.result_correctedKeyword[i].itemCondition])
+				continue
+        let history_count = 0;
+        if (itemList.search_byCorrection_requestsCount !== null) {
+          let temp = itemList.search_byCorrection_requestsCount.filter(
+            x => x.itemID == itemList.result_correctedKeyword[i].itemID
+          );
+          if (temp.length > 0) history_count = temp[0]["count"];
+        }
+        let lenderRate = 0;
+        if (itemList.search_byCorrection_lenderRate !== null)
+          lenderRate =
+            itemList.search_byCorrection_lenderRate[
+              itemList.result_correctedKeyword[i].ownerID
+            ];
+        document.getElementById("corrected_results_section_content").innerHTML +=
           '<div class="col-lg-3 col-md-4 col-sm-6"><div class="card"><span class="item_detail_span" style="cursor:pointer" onclick="location.href = \'/items/' +
           itemList.result_correctedKeyword[i].itemID +
           '\'"><img class="card-img-top img-thumbnail" style="width:100%;height:250px; border-top-width: 0px; border-right-width: 0px;border-bottom-width: 0px;border-left-width: 0px;padding-top: 0px;padding-right: 0px;padding-bottom: 0px;padding-left: 0px;" src="' +
@@ -280,13 +367,21 @@ let item_render = itemList => {
           condition_pill[itemList.result_correctedKeyword[i].itemCondition] +
           '<div class="card-body" style="padding-left: 10px;padding-bottom: 10px;padding-top: 10px;"><div style="line-height: 1.5em; height: 3em;overflow: hidden;"><strong class="card-title text-dark">' +
           itemList.result_correctedKeyword[i].name +
-          '</strong></div><small class="text-dark"><u>Borrowed by '+history_count+' user(s)</u></small></div><hr style="margin-top:0;margin-bottom:0;" /></span><div class="card-body user_detail_span" onclick="location.href = \'/public_profiles/'+itemList.result_correctedKeyword[i].ownerID+'\';" style="cursor: pointer;padding-left: 10px;padding-bottom: 2px;padding-top: 5px;">' +
+          '</strong></div><small class="text-dark"><u>Borrowed by ' +
+          history_count +
+          ' user(s)</u></small></div><hr style="margin-top:0;margin-bottom:0;" /></span><div class="card-body user_detail_span" onclick="location.href = \'/public_profiles/' +
+          itemList.result_correctedKeyword[i].ownerID +
+          '\';" style="cursor: pointer;padding-left: 10px;padding-bottom: 2px;padding-top: 5px;">' +
           '<span><img class="img-thumbnail" style="width:30px; height:30px; border-radius: 50%; padding-top:0; padding-bottom:0; padding-left:0; padding-right:0;" src="' +
-          itemList.search_byCorrection_lenderPhoto[itemList.result_correctedKeyword[i].ownerID] +
+          itemList.search_byCorrection_lenderPhoto[
+            itemList.result_correctedKeyword[i].ownerID
+          ] +
           '"/>&nbsp; <label class="text-muted">' +
           itemList.result_correctedKeyword[i].ownerName +
           "</label>" +
-          '<label class="pull-right text-warning" style="margin-top:1.5px;"><i class="fa fa-star"></i> '+lenderRate.toFixed(1)+'</label></span></div></div></div>';
+          '<label class="pull-right text-warning" style="margin-top:1.5px;"><i class="fa fa-star"></i> ' +
+          lenderRate.toFixed(1) +
+          "</label></span></div></div></div>";
         totalCount++;
       }
       document.getElementById("corrected_results_section").style.display = "";
