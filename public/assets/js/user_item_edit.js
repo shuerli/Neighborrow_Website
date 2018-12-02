8493b50babdc058b446c3d30d096ded2229eff41
Zeyu Feng <zeyu.feng@mail.utoronto.ca>
2018-12-01 20:13:15 -0500

    Changed js user item related js files to correctly update item info wrt view template changes

--- public/assets/js/user_item_edit.js
+++ public/assets/js/user_item_edit.js
@@ -108,19 +108,14 @@ $('#department-input').change(function () {
     itemId = curLoc[curLoc.length - 1];
 
 
-    var radio_button = document.forms[0];
-    var i;
-    var item_condition;
-    for(i = 0; i < radio_button.length; i++) {
-        if(radio_button[i].checked){
-            item_condition = radio_button[i].value;
-        }
-    }
+
+    var conditionList = document.getElementById('condition-input');
+    var item_condition = conditionList.options[conditionList.selectedIndex].value;
+
     var sdate = moment(document.getElementById('date-input').value.substring(0,10) , 'MM/DD/YYYY');
     sdate = moment(sdate).format('YYYY-MM-DD') + " 00:00:00"
     var edate = moment(document.getElementById('date-input').value.substring(13,23) , 'MM/DD/YYYY');
     edate = moment(edate).format('YYYY-MM-DD') + " 00:00:00"
-
     $.ajax({
         url: "/category/id",
         method: "GET",
