3746df57f67a0db6b4da71e4ba7d2ca118d065d4
lillizhuo <qingqing.zhuo@mail.utoronto.ca>
2018-12-04 02:58:49 -0500

    update team page

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -37,18 +37,5 @@ RailsAdmin.config do |config|
     ## With an audit adapter, you can add:
     # history_index
     # history_show
-	end
-	
-	config.parent_controller = '::ApplicationController'
-
-	config.authorize_with do |controller|
-    unless current_user.role == 'admin'
-      redirect_to "/404"
-    end
-	end
-
-	# Link for handling the reports (backup page for the database manipulation)
-	#config.navigation_static_links = {
-  #	'Report List' => '/admin_reports'
-	#}
+  end
 end
